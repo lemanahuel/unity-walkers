06dabf3e3dd5b6b2c30e3a6d5a547c9afc0180f6
nahuellema <lema.nahuel@gmail.com>
2017-05-07 13:23:28 -0300

    IMPROVE, waypoints y script de seek

---
+++ Assets/Scripts/Civilian.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Civilian : MonoBehaviour {
+	GameObject target;
+	List<GameObject> waypoints = new List<GameObject>();
+	public float speed = 10;
+    float rotationSpeed = 0.25f;
+    Vector3 endPosition;
+
+	// Use this for initialization
+	void Start () {
+		this.setWaypoints();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		target = getRandomWaypoint();
+		Seek();
+	}
+
+	void setWaypoints(){
+		var items = GameObject.FindGameObjectsWithTag("waypoint");
+
+		foreach (var item in items) {
+			waypoints.Add(item);
+		}
+	}
+
+	GameObject getRandomWaypoint(){
+		int rand = Random.Range(0, waypoints.Count);
+		return waypoints[rand];
+	}
+
+    void Seek() {
+		endPosition = target.transform.position - transform.position;
+		transform.forward = Vector3.Lerp(transform.forward, endPosition, rotationSpeed * Time.deltaTime);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+}
